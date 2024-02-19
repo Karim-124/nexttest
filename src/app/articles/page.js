92eaf81ebbe258c47d2822890f3a6130e6e58931
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-02-19 23:32:52 +0200

    learning routing

---
+++ src/app/articles/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+
+function test() {
+  return(
+     <div>
+      <h2>Post Page</h2>
+      <Link href="/posts">
+        go to the post page 
+      </Link>
+
+  </div>
+  )
+  
+}
+
+export default test;
